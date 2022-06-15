57cb637c3788b7ff99c70f5ac5c9ecfd9c9faee4
shafucheng <flynn.sha@outlook.com>
2022-06-15 18:22:21 +0800

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import logo from './logo.svg'
-import './App.css'
 import { Button, message } from 'antd'
 
 function showMessage() {
@@ -8,25 +6,9 @@ function showMessage() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type="primary" onClick={showMessage}>
-          按钮
-        </Button>
-      </header>
-    </div>
+    <Button type="primary" onClick={showMessage}>
+      按钮
+    </Button>
   )
 }
 
